e3d2b0fecb5d9b63f243a2019024559b541e6081
Jing <jing20777@gmail.com>
2025-12-13 16:11:19 +0800

    feat: add temperature history list with data class

--- app/src/main/java/com/example/thermometer/MainActivity.kt
+++ app/src/main/java/com/example/thermometer/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.ColorInt
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -21,6 +25,13 @@ import androidx.compose.ui.unit.dp
 import com.example.thermometer.ui.theme.ThermometerTheme
 import androidx.compose.runtime.*
 
+
+data class Temperature(
+    val celsius: Double
+){
+    val fahrenheit: Double get() = celsius * 9.0 / 5.0 + 32.0
+}
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,46 +43,53 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-//@Preview(showBackground = true, name = "状态提升示例")
-@Composable
-fun ThermometerInputField(
-    celsiusInput: String,
-    onCelsiusChange: (String) -> Unit,
-    fahrenheit: Double
-
-) {
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize() //沾满屏幕
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        TextField(
-            value = celsiusInput,
-            onValueChange = onCelsiusChange,
-            label = {Text("摄氏度")},
-            placeholder = {Text("例如: 36.5")}
-        )
-
-        Text(
-            text = "华氏度: $fahrenheit°F",
-            modifier = Modifier.padding(top = 16.dp)
-        )
-    }
-}
-
+@Preview(showBackground = true, name = "状态提升示例")
 
 @Composable
 fun ThermometerScreen(){
     var celsiusInput by remember { mutableStateOf("0") }
-    val celsius = celsiusInput.toDoubleOrNull() ?: 0.0
-    val fahrenheit = celsius * 9.0 / 5.0 + 32.0
+    val celsius = celsiusInput.toDoubleOrNull() ?:0.0
 
-    ThermometerInputField(
-        celsiusInput = celsiusInput,
-        onCelsiusChange = { celsiusInput = it },
-        fahrenheit = fahrenheit
-    )
+    val temperatureHistory = remember {
+        mutableStateListOf<Temperature>()
+    }
+
+    LaunchedEffect(celsius) {
+        if (temperatureHistory.none { it.celsius == celsius }){
+            temperatureHistory.add(0, Temperature(celsius))
+        }
+    }
+
+    Column (
+        modifier = Modifier.fillMaxSize() //沾满屏幕
+    ){
+       TextField(
+           value = celsiusInput,
+           onValueChange = {celsiusInput = it },
+           label = {Text("摄氏度")},
+           modifier = Modifier.padding(16.dp)
+       )
+
+        LazyColumn (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+        ){
+            items(temperatureHistory){ temp ->
+                TemperatureItem(temp)
+
+        }
+        }
+    }
+}
+@Composable
+fun TemperatureItem(temperature: Temperature) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text("摄氏度: ${temperature.celsius}°C")
+        Text("华氏度: ${temperature.fahrenheit}°F")
+    }
 }
